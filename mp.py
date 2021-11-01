9eb54f5339fcc8af3c5843a465314fc03f919d9a
dpstart <danielepaliotta96@gmail.com>
2021-11-01 15:01:25 +0100

    improve mp

--- mp.py
+++ mp.py
@@ -2,27 +2,30 @@ import torch
 import torch.nn.functional as F
 
 from torch_geometric.nn import GCNConv
-from torch_scatter import scatter_sum
+from torch_scatter import scatter_mean
 
 
 class GCNZinc(torch.nn.Module):
     def __init__(self):
         super(GCNZinc, self).__init__()
 
-        self.emb = torch.nn.Embedding(28, 64)
-        self.edge_emb = torch.nn.Embedding(4, 16)
-        self.conv1 = GCNConv(64, 64)
-        self.conv2 = GCNConv(64, 64)
-        self.lin = torch.nn.Linear(64, 1)
+        self.emb = torch.nn.Embedding(28, 128)
+        self.edge_emb = torch.nn.Embedding(32, 1)
+        self.conv1 = GCNConv(128, 128)
+        self.conv2 = GCNConv(128, 128)
+        self.lin1 = torch.nn.Linear(128, 64)
+        self.lin2 = torch.nn.Linear(64, 1)
 
     def forward(self, data, get_emb=False):
         x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
         x = self.emb(x.squeeze(-1))
-        # edge_weight = self.edge_emb(edge_weight)
+        # edge_weight = self.edge_emb(edge_weight).squeeze(-1)
         x = F.relu(self.conv1(x, edge_index))
         x = F.dropout(x, training=self.training)
-        emb = self.conv2(x, edge_index)
-        x = self.lin(emb)
+        emb = F.relu(self.conv2(x, edge_index))
+        x = F.relu(scatter_mean(emb, data.batch, dim=0))
+        x = F.relu(self.lin1(x))
+        x = self.lin2(x)
 
-        return emb if get_emb is True else scatter_sum(x, data.batch, dim=0).squeeze(-1)
+        return emb if get_emb is True else x.squeeze(-1)
 
